50a2cc61efb6d83744ab01d6e423482d4f5a23e9
SD <denisov.univ@gmail.com>
2017-10-04 13:13:55 +0300

    Added threads tests

--- main.cpp
+++ main.cpp
@@ -5,14 +5,48 @@
 #include "MrCat.h"
 #include <iostream>
 
+#include <thread>
+
 //denisov.univ@gmail.com
 //sergvic
 //sireukr
 
 using namespace std;
 
+void testThread() {
+	while (true) {
+		int i = 5;
+		double a = 4;
+		while (i<2000000000) {
+			i += a*a;
+			a -= i;
+		}
+		cout << ".";
+		this_thread::sleep_for(3s);
+	}
+}
+
 void main()
 {
+	thread t1(testThread);
+	thread t2(testThread);
+	thread t3(testThread);
+	while (true) {
+		int i = 5;
+		double a = 4;
+		while (i<200000000) {
+			i += a*a;
+			a -= i;
+		}
+		cout << "*";
+		this_thread::sleep_for(2s);
+	}
+	t1.join();
+	cout << "Thread 1 finished!" << endl;
+	t2.join();
+	t3.join();
+	return;
+	
 	MrCat cafe;
 	cafe.showMenu();
 
